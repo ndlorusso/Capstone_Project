974137e86e951d644450e6503a3cae9cb0758678
Nick Lorusso <ndlorusso@gmail.com>
2024-05-21 11:01:02 -0500

    file setup

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/your-database-name';
+const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/capstone_project_db';
 
 const db = new Client({
     connectionString,
