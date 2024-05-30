7c84420edfb47855bf071ee2fc83e4b88c9b279c
Brendan Smith <92564077+brendansmith123@users.noreply.github.com>
2024-05-29 19:28:42 -0500

    Update shoes.js

--- src/server/db/shoes.js
+++ src/server/db/shoes.js
@@ -1,36 +1,53 @@
+const id = require("volleyball/lib/id");
 const db = require("./client");
-const uuid = require("uuid");
+const uuid = require('uuid');
 
-const createShoe = async ({ brand, size, price, color }) => {
-  const SQL = `--sql
+// TEST FOR API.ROUTER
+const express = require('express');
+const apiRouter = express.Router();
+
+// const { Client } = require("pg");
+
+// CREATE FUNCTION
+const createShoe = async ({ brand, size, price, color}) => {
+    const SQL = `--sql
     INSERT INTO shoes(id, brand, size, price, color)
     VALUES ($1, $2, $3, $4, $5)
     RETURNING *
     `;
-  const response = await db.query(SQL, [uuid.v4(), brand, size, price, color]);
-  return response.rows[0];
-};
-const updateShoe = async ({ id, brand, size, price, color }) => {
+    const response = await db.query(SQL, [uuid.v4(), brand, size, price, color]);
+    return response.rows[0];
+  };
+
+// READ ALL SHOES
+const fetchAllShoes = async () => {
   const SQL = `--sql
-      UPDATE shoes
-      SET brand = $2, size = $3, price = $4, color = $5
-      WHERE id = $1
-      RETURNING *
-      `;
-  const response = await db.query(SQL, [id, brand, size, price, color]);
-  return response.rows[0];
+  SELECT * from shoes
+  `;
+  const response = await db.query(SQL);
+  return response.rows;
 };
 
-const deleteShoe = async (shoeId) => {
-  const SQL = `--sql
-      DELETE FROM shoes
-      WHERE id = $1
-      `;
-  await db.query(SQL, [shoeId]);
-};
+apiRouter.get('/api/shoes', async (req, res, next) => {
+  try {
+      res.send(await fetchAllShoes());
+  } catch (error) {
+      next(error);
+  }
+});
 
-module.exports = {
-  createShoe,
-  updateShoe,
-  deleteShoe,
-};
+// READ SINGLE SHOE
+// const fetchOneShoe = async (id) => {
+//   const SQL = `--sql
+//   SELECT * from SHOES
+//   WHERE id = $1
+//   `;
+//   const response = await db.query(SQL, [id]);
+//   return response.rows;
+// };
+
+  module.exports = {
+    createShoe,
+    fetchAllShoes,
+    // fetchOneShoe,
+  };
