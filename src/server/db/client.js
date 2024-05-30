5e36cc5e3ed9955e13cca8a936e2f65e6db45f77
Brendan Smith <92564077+brendansmith123@users.noreply.github.com>
2024-05-30 09:48:59 -0500

    Update client.js

--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,13 +1,9 @@
-const { Client } = require("pg");
-const connectionString =
-  process.env.DATABASE_URL || "http://localhost:5432/capstone_project_db";
+const { Client } = require('pg');
+const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/capstone_project_db';
 
 const db = new Client({
-  connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : undefined,
+    connectionString,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = db;
