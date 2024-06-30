4750fd4f09fdcbdf78c8caebaffec2cde894e539
Nickolai Venturini <nicktellah5600@yahoo.com>
2024-06-30 11:18:14 -0400

    nickolai_branch

--- src/client/components/SuccessPage.jsx
+++ src/client/components/SuccessPage.jsx
@@ -1,17 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
 
-
 const SuccessPage = () => {
-const navigate = useNavigate();
-    function sendMeHome () {
+    const navigate = useNavigate();
+
+    function sendMeHome() {
         navigate('/');
     };
 
-return <>
-    <h1>Thank you for your order!</h1>
-    <button onClick={sendMeHome}>Home</button>
-    </>
+    return (
+        <div className="success-page">
+            <h1>Thank you for your order!</h1>
+            <button onClick={sendMeHome} className="home-button">Home</button>
+        </div>
+    );
 };
 
 export default SuccessPage;
